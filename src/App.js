7484c59f9c9fac8c16ebbff3e91f09f7a39a5167
Prasanth <1022prasanthkumar@gmail.com>
2023-06-27 16:30:49 +0530

    added new pages

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
