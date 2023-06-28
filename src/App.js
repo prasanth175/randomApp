b9bc7f8d8b8c8911c17e8afe2fc77b71c3cb73ae
Prasanth <1022prasanthkumar@gmail.com>
2023-06-28 10:24:42 +0530

    edited unmerged files

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-
         <Route path='/cart' element={<Cart />} />
       </Routes>
     </div>
